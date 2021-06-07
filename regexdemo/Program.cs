2ab0c2f7ce8f490741c95855c3c20509d2329f46
lahari <laharipalakurthi135@gmail.com>
2021-06-07 14:57:49 +0530

    test

---
+++ regexdemo/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace regexdemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CallAgain:
+                Console.WriteLine("Enter userName : ");
+                string userName = Console.ReadLine();
+
+
+
+                string smallCharPattern = "^[a-z]+$";
+                string capitalCharPattern = "^[A-Z]+$";
+                string onlyCharPattern = "^[a-zA-Z]+$";
+
+
+
+                string onlyNumberPatter = "^[0-9]+$";
+
+
+
+                string alphanumPatter = "^[a-zA-Z0-9]+$";
+
+
+
+                string MobilePattern = "^[0-9]{10}$";
+
+
+
+                Regex rx = new Regex(@"^((\+){0,1}91(\s){0,1}(\-){0,1}(\s){0,1}){0,1}9[0-9](\s){0,1}(\-){0,1}(\s){0,1}[1-9]{1}[0-9]{7}$"); ;
+                Match resVal = rx.Match(userName);
+
+
+
+                Regex regex = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+                Match match = regex.Match(userName);
+            if (match.Success)
+            {
+                Console.WriteLine("Good Email id. ");
+            }
+            //bool res = Regex.IsMatch(userName, IndianFormatMobilePattern);
+            if (resVal.Success)
+            {
+                Console.WriteLine("Thanks for the userName. ");
+            }
+            else
+            {
+                    Console.WriteLine("please Enter a proper userName only with string. ");
+                    goto CallAgain;
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+
--- regexdemo/Properties/AssemblyInfo.cs
+++ regexdemo/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("test1")]
+[assembly: AssemblyTitle("regexdemo")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("test1")]
+[assembly: AssemblyProduct("regexdemo")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9c2787f3-8147-4860-a33e-29a3cbc64611")]
+[assembly: Guid("2ff3cc98-8507-44d9-8d01-b92c38b645c1")]
 
 // Version information for an assembly consists of the following four values:
 //
